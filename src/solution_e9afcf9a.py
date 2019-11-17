6fe53bf53b17f468ba6af02942e53fc552bb1e8d
Ankit Agarwal <a.agarwal2@nuigalway.ie>
2019-11-17 19:44:40 +0000

    completed the other solution

--- src/solution_e9afcf9a.py
+++ src/solution_e9afcf9a.py
@@ -6,20 +6,22 @@ filename = 'e9afcf9a.json'
 
 
 def process_data(*args):
-    output = []
+    output_data = []
     for training_data in data['train']:
-        array = to_numpy_array(training_data['input'])
-        output_grid = to_output_array(array)
-        n_rows, n_cols = np.shape(output_grid)
-        for i in range(n_rows-1):
-            for j in range(n_cols):
-                output_grid[i, j] = array[i+1, j]
-        output.append(output_grid.tolist())
-    return output, data['test']
+        input_grid = to_numpy_array(training_data['input'])
+        test_grid = to_numpy_array(training_data['output'])
+        output_grid = to_output_array(input_grid)
+        _, n_cols = np.shape(output_grid)
+        for i in range(n_cols):
+            if (i % 2):
+                output_grid[0, i] = input_grid[1, i]
+                output_grid[1, i] = input_grid[0, i]
+        if np.array_equal(output_grid, test_grid):
+            output_data.append(output_grid.tolist())
+    return output_data
 
 
 # This is main handler for the proram
 if __name__ == "__main__":
     data = load_json(filename)
-    output_grid, test = process_data(data)
-    print(output_grid, test)
+    output_grid = process_data(data)
