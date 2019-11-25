92e0a852c6f821dcdb5a43fbf4feff594608321d
Ankit Agarwal <a.agarwal2@nuigalway.ie>
2019-11-25 17:07:48 +0000

    second solution

--- src/solution_e9afcf9a.py
+++ src/solution_e9afcf9a.py
@@ -1,27 +1,27 @@
-from utils import *
-import numpy as np
+from utils import ProcessData, to_output_array, \
+    get_shape_of_array, get_unique_items, output_to_json
 
 
-filename = 'e9afcf9a.json'
-
-
-def process_data(*args):
-    output_data = []
-    for training_data in data['train']:
-        input_grid = to_numpy_array(training_data['input'])
-        test_grid = to_numpy_array(training_data['output'])
-        output_grid = to_output_array(input_grid)
-        _, n_cols = np.shape(output_grid)
-        for i in range(n_cols):
-            if (i % 2):
-                output_grid[0, i] = input_grid[1, i]
-                output_grid[1, i] = input_grid[0, i]
-        if np.array_equal(output_grid, test_grid):
-            output_data.append(output_grid.tolist())
-    return output_data
+def solve(input_grid):
+    """
+    Here we have solve function that takes the input grid
+    and produces the output grid based on the below logic.
+    """
+    _output_grid = to_output_array(input_grid)
+    _, n_cols = get_shape_of_array(input_grid)
+    for i in range(n_cols):
+        if (i % 2):
+            _output_grid[0, i] = input_grid[1, i]
+            _output_grid[1, i] = input_grid[0, i]
+    return output_to_json(_output_grid)
 
 
 # This is main handler for the proram
 if __name__ == "__main__":
-    data = load_json(filename)
-    output_grid = process_data(data)
+    pd = ProcessData(__file__)
+    for idx in range(len(pd.input_grid)):
+        output = solve(pd.input_grid[idx])
+        print(output + '\n')
+    for idx in range(len(pd.eval_grid)):
+        output = solve(pd.eval_grid[idx])
+        print(output)
