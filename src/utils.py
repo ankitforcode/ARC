ca1af78c412442202ed60cce08d25bf83882ba6e
mnanda-g <57842575+mnanda-g@users.noreply.github.com>
2019-11-25 12:12:23 +0000

    updated def function names, compare outputs; converted numpy array output to Json

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
+#Import libraries
 import json
 import os
 import re
 import numpy as np
 
 class ProcessData():
+
+    #init method or constructor
     def __init__(self, file):
         """
         These are set of initial variables that are 
@@ -17,6 +20,15 @@ class ProcessData():
         self._get_json_file()
         self._load_json()
         self._get_grids_from_json()
+
+    def _get_json_file(self):
+        """
+        This functions gets the json filename from the solution
+        filename based on the realpath of current work dir.
+        """
+        dir_path = os.path.dirname(os.path.realpath(self.file))
+        f = re.findall(r"[a-z0-9]+\w", self.file)[-2] + '.json'
+        self.filename = dir_path + '/' + f
 
 def load_json(filename):
     """
