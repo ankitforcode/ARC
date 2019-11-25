78c79c078ef178bd7d843eef57e627dc7f396481
mnanda-g <57842575+mnanda-g@users.noreply.github.com>
2019-11-25 12:22:12 +0000

    added changes

--- src/utils.py
+++ src/utils.py
@@ -30,29 +30,62 @@ class ProcessData():
         f = re.findall(r"[a-z0-9]+\w", self.file)[-2] + '.json'
         self.filename = dir_path + '/' + f
 
-def load_json(filename):
+    def _load_json(self):
+        """
+        This function takes the filename as input and 
+        laods the json in to the object for further
+        processing.
+        """
+        with open(self.filename) as json_file:
+            self.data = json.load(json_file)
+
+    def _get_train_grid_from_json(self):
+        """
+        This function reads the input data from the train
+        block and also reads the output data so that we 
+        can test against our generated output.
+        """
+        for d in self.data['train']:
+            self.input_grid.append(np.array(d['input']))
+
+    def _get_eval_grid_from_json(self):
+        for d in self.data['test']:
+            self.eval_grid.append(np.array(d['input']))
+
+def get_unique_items(array):
     """
-    This function takes the filename as imput and 
-    laods the jsobn object in a variables and 
-    returns back.
+    This function gets the unique items
+    from the provided numpy array.
     """
-    with open(filename) as json_file:
-        data = json.load(json_file)
-    return data
-
-
-def to_numpy_array(array):
-    """
-    This function converts the list type to numpy
-    array for further processing of the data.
-    """
-    arr = np.array(array)
-    return arr
-
-
-def find_unique(array):
-    return np.unique(array)
-
+    unique_items = np.unique(array)
+    return unique_items
 
 def to_output_array(array):
-    return np.copy(array)
+    """
+    This function copied the provided numpy
+    array to another array.
+    """
+    cloned_array = np.copy(array)
+    return cloned_array
+
+def get_shape_of_array(array):
+    """
+    This function returns the shape of the
+    numpy array.
+    """
+    n_row, n_column = np.shape(array)
+    return n_row, n_column
+
+def compare_np_array(arr1, arr2):
+    """
+    This function compares the two numpy array
+    and returns the bool response.
+    """
+    return np.array_equal(arr1, arr2)
+
+def output_to_json(arr):
+    """
+    This function converts the provided numpy
+    array to json object and returns back.
+    """
+    return json.dumps({'output': arr.tolist()})
